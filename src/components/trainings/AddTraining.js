1e8e1512e8b86942b9cb08fb066fcf3f05a2d631
keep1tstupid <alexandra.globa@gmail.com>
2021-03-17 14:30:30 +0200

    started working on validation - locking save button until every field is filled

--- src/components/trainings/AddTraining.js
+++ src/components/trainings/AddTraining.js
@@ -5,7 +5,6 @@ import { fetchAllCustomers, addNewTraining } from "../../redux/actions";
 
 const AddTraining = (props) => {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(fetchAllCustomers());
   }, [dispatch]);
@@ -14,7 +13,7 @@ const AddTraining = (props) => {
     date: '',
     duration: '',
     activity: '',
-    customer: '',
+    customer: 'none',
   }
 
   // •date (Date)
@@ -23,37 +22,72 @@ const AddTraining = (props) => {
   // •Customer (customer.id) >> customer link
 
   const [show, setShow] = useState(false);
+  const [saveButtonAvailable, setSaveButtonAvailable] = useState(false);
   const [training, setTraining] = useState(INITIAL_STATE);
+
+  const checkFields = (trainingData) => {
+    setTimeout(() => {
+      if (trainingData.date !== '' &&
+        trainingData.duration !== '' &&
+        trainingData.activity !== '' &&
+        trainingData.customer !== 'none') {
+        console.log('Fields are fine: ', trainingData);
+        setSaveButtonAvailable(true);
+      } else {
+        console.log('Something missed: ', trainingData);
+        setSaveButtonAvailable(false);
+      }
+    }, 1000)
+
+    // if (trainingData.date !== '' &&
+    //   trainingData.duration !== '' &&
+    //   trainingData.activity !== '' &&
+    //   trainingData.customer !== 'none') {
+    //   console.log('Fields are fine: ', trainingData);
+    //   setSaveButtonAvailable(true);
+    // } else {
+    //   console.log('Something missed: ', trainingData);
+    //   setSaveButtonAvailable(false);
+    // }
+  }
 
   const handleShow = () => {
     setShow(true);
+    // setSaveButtonAvailable(false);
   }
   const handleClose = () => {
     setShow(false);
     setTraining(INITIAL_STATE);
+    setSaveButtonAvailable(false);
   }
 
   const handleChange = (event) => {
     setTraining({...training, [event.target.name]: event.target.value});
+    checkFields(training);
   }
 
   const handleCustomerChange = (event) => {
-    // console.log(event);
-    setTraining({
-      ...training, customer: props.customers[event.target.value].links[0].href
-      })
-    // console.log(training.customer);
+    if (event.target.value === 'none') {
+      setTraining({...training, customer: 'none'});
+    } else {
+      setTraining({
+        ...training, customer: props.customers[event.target.value].links[0].href
+      });
+    }
+    //console.log(training.customer);
+    checkFields(training)
   }
 
   const handleSave = () => {
     //console.log(JSON.stringify(training));
-    if (training.date === '' || training.duration === '' || training.activity === '' || training.customer === '') {
+    if (training.date === '' || training.duration === '' || training.activity === '' || training.customer === 'none') {
       console.log('NO!');
     } else {
       dispatch(addNewTraining(training));
     }
     handleClose();
   }
+
 
   return (
     <>
@@ -79,8 +113,6 @@ const AddTraining = (props) => {
                 name='date'
                 onChange={handleChange}
                 value={training.date}
-                required
-                // isInvalid
               />
             </Form.Group>
             <Form.Group as={Col}>
@@ -90,7 +122,6 @@ const AddTraining = (props) => {
                 name='duration'
                 onChange={handleChange}
                 value={training.duration}
-                required
               />
             </Form.Group>
           </Form.Row>
@@ -102,7 +133,6 @@ const AddTraining = (props) => {
                 name='activity'
                 onChange={handleChange}
                 value={training.activity}
-                required
               />
             </Form.Group>
             <Form.Group as={Col}>
@@ -112,9 +142,8 @@ const AddTraining = (props) => {
                 name='customer'
                 onChange={handleCustomerChange}
                 //value={training.customerName}
-                required
               >
-                <option value='1' >Select</option>
+                <option value='none' >Select</option>
                 {props.customers.map((customer, index) =>
                   <option key={index} value={index}>
                     {customer.firstname + ' ' + customer.lastname}
@@ -124,13 +153,27 @@ const AddTraining = (props) => {
           </Form.Row>
 
         </Modal.Body>
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
+
+          { saveButtonAvailable &&
           <Button variant="primary" onClick={handleSave}>
             Save
           </Button>
+          }
+
+          { saveButtonAvailable === false &&
+          <Button variant="primary" disabled>
+            Save
+          </Button>
+          }
+
+          {/*<Button variant="primary" onClick={handleSave}>*/}
+          {/*  Save*/}
+          {/*</Button>*/}
         </Modal.Footer>
       </Modal>
     </>
